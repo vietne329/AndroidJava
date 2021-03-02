c43af574d7dcc19096e877d53c91b5aa04cbd821
vietne329 <mikedukeviet@gmail.com>
2021-03-02 22:37:54 +0700

    add .

---
+++ Test/app/src/main/java/com/example/test/LoginActivitiy.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivitiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activitiy);
+        
+    }
+}
